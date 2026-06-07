ddcaa8b0b6cdc86a09a6c7a2240894b5395070e0
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-06-07 16:12:45 -0400

    Add Student class skeleton with fields

---
+++ Student.java
@@ -0,0 +1,17 @@
+/*
+ * Student.java
+ * Author: Christian Jarrell
+ * Course: CS3700
+ * Description: Defines a Student class that stores personal details and up to six registered courses.
+ */
+
+public class Student {
+
+    // Fields
+    private String name;
+    private int age;
+    private String email;
+    private String phoneNumber;
+    private String stage;
+    private Sring[] courses; // up to 6 registered courses
+}
