9bc166f7ddf8bd97769a7e88851bf6c0359b001c
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-06-07 19:11:20 -0400

    Add Maind river with first student

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Main.java
+ * Author: Christian Jarrell
+ * Course: CS3700
+ * Description: Driver class that creates Student objects with different numbers of registered courses and prints
+ * their information
+ */
+
+public Main {
+    public static void main(String[] args) {
+
+        // First student - 6 courses (full load)
+        String[] courses 1 = {"CS3700", "MATH2400", "ENG1010", "HIST1500", "PHYS2100", "ART1000"};
+        Student student1 = new Student("Alice Jones", 20, "alice.j@school.edu", "828-555-0101", "Sophomore", courses1);
+
+        student1.printMe();
+    }
+}
