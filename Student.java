75bb30e4e84df90b4ee728774032fdc89b9801cf
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-06-07 19:04:16 -0400

    Add constructor to initialize Student properties

--- Student.java
+++ Student.java
@@ -14,4 +14,14 @@ public class Student {
     private String phoneNumber;
     private String stage;
     private Sring[] courses; // up to 6 registered courses
+
+    // Constructor that accepts all properties as parameters for object initialization
+    public Student(String name, int age, String email, String phoneNumber, String stage, String[] courses) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.stage = stage;
+        this.courses = courses;
+    }
 }
